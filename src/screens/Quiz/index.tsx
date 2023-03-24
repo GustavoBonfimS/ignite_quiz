57b77b090f73e5dbae3ba4dd21b379a04004c663
Gustavo Bonfim <gustavo.hbonfim@outlook.com>
2023-03-24 14:31:55 -0300

    skip card on drag

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -18,6 +18,7 @@ import Animated, {
   Easing,
   Extrapolate,
   interpolate,
+  runOnJS,
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
@@ -34,6 +35,7 @@ interface Params {
 type QuizProps = typeof QUIZ[0];
 
 const CARD_INCLINATION = 10;
+const CARD_SKIP_AREA = (-200);
 
 export function Quiz() {
   const [points, setPoints] = useState(0);
@@ -127,7 +129,10 @@ export function Quiz() {
         cardPosition.value = event.translationX;
       }
     })
-    .onEnd(() => {
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) {
+        runOnJS(handleSkipConfirm)();
+      }
       cardPosition.value = withTiming(0);
     });
 
